f8889d58b7a0a873341b14ba92464bb317a5c54e
Александра Румянцева <alexrumyantceva.gamil.com@yandex.ru>
2022-04-26 11:51:30 +0300

    Первая проверка 2

--- yatube/posts/tests/test_forms.py
+++ yatube/posts/tests/test_forms.py
@@ -45,12 +45,12 @@ class PostsCreateTests(TestCase):
         """Валидная форма создает запись в базе данных."""
         posts_count = Post.objects.count()
         small_gif = (
-             b'\x47\x49\x46\x38\x39\x61\x02\x00'
-             b'\x01\x00\x80\x00\x00\x00\x00\x00'
-             b'\xFF\xFF\xFF\x21\xF9\x04\x00\x00'
-             b'\x00\x00\x00\x2C\x00\x00\x00\x00'
-             b'\x02\x00\x01\x00\x00\x02\x02\x0C'
-             b'\x0A\x00\x3B'
+            b'\x47\x49\x46\x38\x39\x61\x02\x00'
+            b'\x01\x00\x80\x00\x00\x00\x00\x00'
+            b'\xFF\xFF\xFF\x21\xF9\x04\x00\x00'
+            b'\x00\x00\x00\x2C\x00\x00\x00\x00'
+            b'\x02\x00\x01\x00\x00\x02\x02\x0C'
+            b'\x0A\x00\x3B'
         )
         uploaded = SimpleUploadedFile(
             name='small.gif',
@@ -118,7 +118,7 @@ class PostsCreateTests(TestCase):
             'text': 'Тестовый коммент',
         }
         response = self.authorized_client.post(
-            reverse('posts:add_comment',  kwargs={'post_id': self.post.id}),
+            reverse('posts:add_comment', kwargs={'post_id': self.post.id}),
             data=form_data,
             follow=True
         )
@@ -134,7 +134,7 @@ class PostsCreateTests(TestCase):
             'text': 'Тестовый коммент',
         }
         response = self.guest_client.post(
-            reverse('posts:add_comment',  kwargs={'post_id': self.post.id}),
+            reverse('posts:add_comment', kwargs={'post_id': self.post.id}),
             data=form_data,
             follow=True
         )
